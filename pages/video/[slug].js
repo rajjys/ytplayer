28fc879771b84fb7130ef44ce4da4adbc85070dd
rajjys <rajjysrachid@gmail.com>
2023-07-21 06:23:07 +0200

    Can now display videos. Yet the API is causing us to crash

--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
     const router = useRouter();
     const { slug } = router.query;
+    const [VideoDetail, setVideoDetail] = useState(null);
+    useEffect(() => {
+      fetchFromAPI(`videos?part=snippet,statistics&id=${slug}`)
+      .then((data) => setVideoDetail(data.items[0]));
+    }, []);
   return (
-    <div>VideoDetail
-        <h1>{slug}</h1>
-    </div>
+    <Box minHeight='95vh'>
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${slug}`} className="react-player" controls/>
+          </Box>
+        </Box>
+
+      </Stack>
+    </Box>
   )
 }
 
